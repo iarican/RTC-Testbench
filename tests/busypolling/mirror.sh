79260ce499a64eb19fa78d99f667fc34a1f744b0
Irmak <101280533+iarican@users.noreply.github.com>
2025-06-30 14:57:54 +0200

    Update mirror.sh
    configured the interface 
    
    Signed-off-by: Irmak <101280533+iarican@users.noreply.github.com>

--- tests/busypolling/mirror.sh
+++ tests/busypolling/mirror.sh
@@ -11,11 +11,11 @@ set -e
 cd "$(dirname "$0")"
 
 # Start PTP
-../../scripts/ptp.sh enp3s0
+../../scripts/ptp.sh enp171s0
 sleep 30
 
 # Configure flow
-./flow_cml.sh enp3s0
+./flow_cml.sh enp171s0
 sleep 30
 
 # Start one instance of mirror application
